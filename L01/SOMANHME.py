6170b3ef899617bc8f3a9e6a5f21922b80e4ffe6
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-04-21 19:55:11 +0700

    commit

--- L01/SOMANHME.py
+++ L01/SOMANHME.py
@@ -1,7 +1,8 @@
 import math
 n = 1000
 def is_prime(x):
-    if x < 2: return False
+    if x < 2: 
+        return False
     for i in range(2, int(math.sqrt(x))+1):
         if x % i == 0: 
             return False
