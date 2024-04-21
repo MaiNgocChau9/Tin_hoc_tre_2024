5281bd1decfb96d95c4850e7980ae885abcde394
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-04-21 19:43:59 +0700

    commit

--- L01/SONGUYENTO.py
+++ L01/SONGUYENTO.py
@@ -10,7 +10,7 @@ def is_prime(n):
 
 def is_prime2(n):
     if n < 2: return False
-    for i in range(2, int(math.sqrt(n))):
+    for i in range(2, int(math.sqrt(n)+1)):
         if n % i == 0: 
             return False
     return True
