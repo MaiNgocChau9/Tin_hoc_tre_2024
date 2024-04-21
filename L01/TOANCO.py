07715c5b31cc796293e8bdd52cee81fc1433abc6
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-04-21 21:01:04 +0700

    commti

--- L01/TOANCO.py
+++ L01/TOANCO.py
@@ -2,17 +2,18 @@ input_file = open("TOANCO.inp")
 output_file = open("TOANCO.out")
 
 data = input_file.readlines()[0]
-data.split(" ")
+data = data.split()
+
 tong = int(data[0])
 chan = int(data[1])
 
 ga = cho = 0
 for i in range(tong):
+    print(i)
     ga = i
     cho = tong - ga
     if ga*2 + cho*4 == chan:
-        print("ga",ga)
-        print("cho",cho)
+        output_file.write(str(ga), str(cho))
 
 # Close
 input_file.close()
