159e39d91f3f27a7bed3979ebc91c1d69c41ba04
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-04-21 19:29:33 +0700

    commit

--- L01/SOMANHME.py
+++ L01/SOMANHME.py
@@ -1,6 +1,8 @@
 import math
 a = []
 for i in range(1001):
-    try: if i % math.sqrt(i)==0: a.append(i)
-    except ZeroDivisionError: pass
+    try:
+        if i % math.sqrt(i)==0: a.append(i)
+    except ZeroDivisionError: 
+        pass
 print(a)
