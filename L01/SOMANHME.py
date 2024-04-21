9cea67b9c4d9a9a94cb806c0e4e20bd5b58bf340
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-04-21 19:54:46 +0700

    commit

--- L01/SOMANHME.py
+++ L01/SOMANHME.py
@@ -1,8 +1,15 @@
 import math
-a = []
-for i in range(1001):
-    try:
-        if i % math.sqrt(i)==0: a.append(i)
-    except ZeroDivisionError: 
-        pass
-print(a)
+n = 1000
+def is_prime(x):
+    if x < 2: return False
+    for i in range(2, int(math.sqrt(x))+1):
+        if x % i == 0: 
+            return False
+    return True
+
+# i là số cần kiểm tra
+# j là số nguyên tố nhỏ hơn i
+for i in range(2, n+1):
+    for j in range(2, i):
+        if is_prime(j) and i%j==0 and i%(j**2)==0:
+            print(i, end=" ")
