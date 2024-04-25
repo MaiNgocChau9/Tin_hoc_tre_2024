f783825db6cec1d1ae19755220e5325cc2a12745
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-04-25 19:57:23 +0700

    commit

---
+++ L02/gcd.py
@@ -0,0 +1,15 @@
+inp_file = open("gcd.inp")
+out_file = open("gcd.out", "w")
+lst = list(map(int, inp_file. readline().split()))
+a = lst[0]
+b = lst[1]
+gcd = 1
+
+for i in range(1, max(a, b)+1):
+    if a % i == 0 and b % i == 0:
+        gcd = i
+out_file.write(gcd)
+
+#! Close
+inp_file.close()
+out_file.close()
