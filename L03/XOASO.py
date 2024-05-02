249b0c216046f701aeec95a133346a66a4bb67b0
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-05-02 21:30:39 +0700

    commit

--- L03/XOASO.py
+++ L03/XOASO.py
@@ -1,20 +1,23 @@
-s = "132535234"
-k = 3
+# Xóa một ví trí bất kì khỗi chuỗi => slicing
+def remove_char_at_index(s, index):
+    return s[:index] + s[index + 1:]
 
-#* Lấy danh sách gốc
-danhsachgoc = []
-for i in range(len(s)):
-    danhsachgoc.append(s[i])
+def solution(s, k):
+    i = 0
+    while k > 0 and i < len(s)-1:
+        # Nếu vị trí hiện tại < vị trí kế tiếp
+        if s[i] < s[i+1]:
+            s = remove_char_at_index(s, i)
+            k -= 1
+            i = 0
+        else:
+            i += 1
+    
+    if k>0:
+        s = s[:-k]
+    
+    return s
 
-#* Tạo biến lưu trữ giá trị mới
-lst = []
-for i in range(k):
-    lst.append(min(danhsachgoc))
-    danhsachgoc.remove(min(danhsachgoc))
-
-#* Lấy chuỗi đầu ra
-s = ""
-for i in range(len(danhsachgoc)):
-        s += danhsachgoc[i]
-
-print(s)
+a = str(input())
+k = int(input())
+print(solution(a, k))
