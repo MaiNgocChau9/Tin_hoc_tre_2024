97463a64fd85f17504d84d662c1ece3e246ced2f
Mai Ngọc Châu <chauaurora9@gmail.com>
2024-05-09 19:28:25 +0700

    commit

---
+++ L05/BANHXEO.py
@@ -0,0 +1,21 @@
+open_file = open("BANHXEO.inp")
+out_file = open("BANHXEO.out", "w")
+
+first_line = open_file.readline().split()
+n = int(first_line[0])
+banhxeo = int(first_line[1])
+komua = 0
+
+for i in range(n):
+    a = open_file.readline().split()
+    b = a[0]
+    c = a[1]
+    if b == "-":
+        if banhxeo - int(c) > 0:
+            banhxeo -= int(c)
+        else:
+            komua += 1
+    elif b == "+":
+        banhxeo += int(c)
+
+out_file.write(f"{banhxeo} {komua}")
