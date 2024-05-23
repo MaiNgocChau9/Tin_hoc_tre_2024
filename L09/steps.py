d58262bff3320f6024f6bbbdab91307746bc884a
MaiNgocChau9 <chauaurora9@gmail.com>
2024-05-23 20:20:14 +0700

    commit

--- L09/steps.py
+++ L09/steps.py
@@ -25,4 +25,12 @@ for i in range(k):
 # Initialize
 f = [0]*(n+1)
 f[1] = 1
+#! Cach 1
 out_f.write(str(steps(n)))
+
+#! Cach 2
+for i in range(2, n+1):
+  if isBroken[i] == False:
+    f[i] = (f[i-1] + f[i-2])%1000000007
+
+out_f.write(str(f[n]))
